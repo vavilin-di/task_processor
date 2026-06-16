31f1c82e03c437dd9a3b8d5f5fbd65dae261701c
div <div@nowhe.re>
2026-06-16 08:59:47 +0700

    [+] Модели SQLAlchemy.

---
+++ src/database/models/tasks.py
@@ -0,0 +1,28 @@
+__all__ = ["Task"]
+
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import true
+from sqlalchemy.sql.functions import func
+from sqlalchemy.sql.sqltypes import JSON, Boolean, DateTime, String
+
+from src.enums import TaskPriority, TaskStatus
+
+from .base import Base
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(), nullable=False)
+    description: Mapped[str] = mapped_column(String(), nullable=False)
+    priority: Mapped[TaskPriority] = mapped_column(String(), nullable=False, default=TaskPriority.MEDIUM.value)
+    status: Mapped[TaskStatus] = mapped_column(String(), nullable=False, default=TaskStatus.NEW.value)
+    created_at: Mapped[datetime] = mapped_column(DateTime(), nullable=False, server_default=func.now())
+    started_at: Mapped[datetime] = mapped_column(DateTime(), nullable=False, server_default=func.now())
+    finished_at: Mapped[datetime] = mapped_column(DateTime(), nullable=True)
+    result: Mapped[dict] = mapped_column(JSON(), nullable=True)
+    errors: Mapped[list[str] | None] = mapped_column(JSON(), nullable=True)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False, server_default=true())
