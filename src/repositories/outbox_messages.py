66e60e5e8fcc791da2702923b45bf4c8d9c909f7
div <div@nowhe.re>
2026-06-27 03:42:25 +0700

    Исправлен метод получения неопубликованных задач: теперь получает и payload, также теперь обёрнут в объект; Исправлено обновление списка ошибок.

--- src/repositories/outbox_messages.py
+++ src/repositories/outbox_messages.py
@@ -1,32 +1,37 @@
 from sqlalchemy import not_, select
-from sqlalchemy.sql.functions import count
 
-from src.database.models.outbox_messages import OutboxMessage
+from src.database.models.outbox_messages import OutboxMessage as OutboxMessageModel
+from src.schemas.outbox_messages import OutboxMessage
 
 from .sqlalchemy_repository import SQLAlchemyRepository
 
 MAX_PUBLISH_ERRORS_COUNT = 5
 
 
-class OutboxMessageRepository(SQLAlchemyRepository[OutboxMessage]):
-    async def get_not_published_task_ids(self, limit: int = 10) -> list[tuple[int, str]]:
+class OutboxMessageRepository(SQLAlchemyRepository[OutboxMessageModel]):
+
+    async def get_not_published_tasks(self, limit: int = 10) -> list[OutboxMessage]:
         statement = (
-            select(OutboxMessage.id, OutboxMessage.routing_key)
-            .where(not_(OutboxMessage.is_published), not_(OutboxMessage.is_failed))
-            .order_by(OutboxMessage.created_at.asc())
+            select(OutboxMessageModel.id, OutboxMessageModel.routing_key, OutboxMessageModel.payload)
+            .where(not_(OutboxMessageModel.is_published), not_(OutboxMessageModel.is_failed))
+            .order_by(OutboxMessageModel.created_at.asc())
             .limit(limit)
             .with_for_update(skip_locked=True)
         )
-        return list((await self._session.execute(statement)).t.all())
+        return [
+            OutboxMessage(id=id, routing_key=routing_key, payload=payload)
+            for id, routing_key, payload in (await self._session.execute(statement)).t.all()
+        ]
 
     async def mark_task_as_published(self, task_id: int) -> None:
-        await self.update(task_id, **{str(OutboxMessage.is_published): True})
+        await self.update(task_id, is_published=True)
 
     async def add_error(self, task_id: int, error: str) -> None:
-        errors_count = await self.get_value(task_id, count(OutboxMessage.errors))
-        if errors_count is None:
-            raise AssertionError(f"errors_count is None для задачи с id={task_id}")
-        if errors_count >= MAX_PUBLISH_ERRORS_COUNT:
-            await self.update(task_id, **{str(OutboxMessage.is_failed): True})
+        message = await self.get(task_id)
+        if message is None:
+            raise AssertionError(f"OutboxMessage с id={task_id} не найдено")
+        message.errors.append(error)
+        if len(message.errors) >= MAX_PUBLISH_ERRORS_COUNT:
+            await self.update(task_id, errors=message.errors, is_failed=True)
             return
-        await self.update(task_id, **{str(OutboxMessage.errors): OutboxMessage.errors.concat(error)})
+        await self.update(task_id, errors=message.errors)
