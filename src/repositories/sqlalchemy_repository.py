af4c6a338a7dd044316e6f6d33f8c2d1dbfae0d6
div <div@nowhe.re>
2026-06-27 03:40:47 +0700

    Добавлен учёт диапазонов дат. Исправлено обновление. Добавлен метод поиска наличия объекта.

--- src/repositories/sqlalchemy_repository.py
+++ src/repositories/sqlalchemy_repository.py
@@ -1,8 +1,9 @@
+import operator
 from itertools import chain
 from typing import Any, Protocol
 
 from sqlakeyset.asyncio import select_page
-from sqlalchemy import and_, select, update
+from sqlalchemy import Select, and_, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import InstrumentedAttribute, Mapped
 from sqlalchemy.sql.functions import GenericFunction
@@ -11,6 +12,9 @@ from sqlalchemy.sql.functions import GenericFunction
 class IDisableable(Protocol):
     id: Mapped[int]
     is_active: Mapped[bool]
+
+
+_FILTER_OPERATORS: tuple[tuple[str, Any], ...] = (("_from", operator.ge), ("_to", operator.le))
 
 
 class SQLAlchemyRepository[ModelT: IDisableable]:
@@ -29,21 +33,43 @@ class SQLAlchemyRepository[ModelT: IDisableable]:
         select_statement = select(self._model).where(self._model.id == record_id, self._model.is_active)
         return (await self._session.execute(select_statement)).scalar_one_or_none()
 
+    async def exists(self, record_id: int) -> bool:
+        exists_statement = select(
+            select(self._model).where(self._model.id == record_id, self._model.is_active).exists()
+        )
+        return bool((await self._session.execute(exists_statement)).scalar())
+
     async def get_all(
         self, cursor: str | None, limit: int, filters: dict[str, Any] | None
     ) -> tuple[list[ModelT], str, bool]:
         select_statement = select(self._model).where(self._model.is_active)
         if filters is not None:
-            select_statement = select_statement.where(
-                and_(
-                    *(
-                        getattr(self._model, filter_field_name) == filter_field_value
-                        for filter_field_name, filter_field_value in filters.items()
-                    ),
-                ),
-            )
+            select_statement = self._get_filtered_statement(select_statement, filters)
         page = await select_page(self._session, select_statement, limit, page=cursor)
         return list(chain.from_iterable(page)), page.paging.bookmark_next, page.paging.has_next
+
+    def _get_filtered_statement(
+        self, select_statement: Select[tuple[ModelT]], filters: dict[str, Any]
+    ) -> Select[tuple[ModelT]]:
+        conditions = []
+        for filter_field_name, filter_field_value in filters.items():
+            if filter_field_value is None:
+                continue
+
+            for filter_suffix, filter_operator in _FILTER_OPERATORS:
+                if not filter_field_name.endswith(filter_suffix):
+                    continue
+                field_name = filter_field_name[: -len(filter_suffix)]
+                field = getattr(self._model, field_name, None)
+                operation = filter_operator
+                break
+            else:
+                operation = operator.eq
+                field = getattr(self._model, filter_field_name, None)
+
+            if field is not None and isinstance(field, InstrumentedAttribute):
+                conditions.append(operation(field, filter_field_value))
+        return select_statement.where(and_(*conditions)) if conditions else select_statement
 
     async def get_value[FieldT](
         self, record_id: int, field: InstrumentedAttribute[FieldT] | GenericFunction[FieldT]
@@ -55,13 +81,15 @@ class SQLAlchemyRepository[ModelT: IDisableable]:
         return (await self._session.execute(stmt)).scalar_one_or_none()
 
     async def update(self, record_id: int, **kwargs: object) -> ModelT | None:
-        object_from_db = await self.get(record_id)
-        if object_from_db is None:
+        is_object_exist = await self.exists(record_id)
+        if not is_object_exist:
             return None
-        update_statement = update(self._model).where(self._model.id == record_id).values(**kwargs)
-        await self._session.execute(update_statement)
+        update_statement = (
+            update(self._model).where(self._model.id == record_id).values(**kwargs).returning(self._model)
+        )
+        update_result = await self._session.execute(update_statement)
         await self._session.flush()
-        return await self.get(record_id)
+        return update_result.scalar_one_or_none()
 
     async def delete(self, record_id: int) -> None:
         update_statement = update(self._model).where(self._model.id == record_id).values(is_active=False)
