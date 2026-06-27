0e8dea9b03e4d9f41e2b7abadabb8be88b7140bc
div <div@nowhe.re>
2026-06-28 03:56:52 +0700

    Добавлен перехват возможных IntegrityError.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from dishka.integrations.fastapi import setup_dishka
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from sqlalchemy.exc import IntegrityError
 
 from .di import create_di_container
 from .routers import tasks_router
@@ -18,3 +20,11 @@ base_router.include_router(version_router)
 app.include_router(base_router)
 
 setup_dishka(container=create_di_container(), app=app)
+
+
+@app.exception_handler(IntegrityError)
+async def integrity_error_handler(request: Request, exc: IntegrityError) -> JSONResponse:
+    return JSONResponse(
+        status_code=status.HTTP_409_CONFLICT,
+        content={"detail": "Конфликт данных. Возможно, связанная запись была удалена."},
+    )
